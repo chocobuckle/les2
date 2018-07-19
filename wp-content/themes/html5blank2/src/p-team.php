0cd8aa535815f0cc418931cc8a756f18512647a7
Conor Doyle <contact@conordoyle.io>
2018-07-19 15:12:34 +0100

    Image tweaks

--- wp-content/themes/html5blank2/src/p-team.php
+++ wp-content/themes/html5blank2/src/p-team.php
@@ -2,7 +2,7 @@
          <!-- =====================================
         ==== Start Team -->
         <section class="team" id="OurPeople" style="padding-top: 100px;" data-scroll-index="1">
-            <div class="container" style="width: 60%">
+            <div class="container" style="width: 90%">
                 <div class="row">
 
                     <div class="section-head col-sm-12">
@@ -22,46 +22,48 @@
                                 $position_title = get_sub_field('position_title', 'option');
                                 $description = get_sub_field('description', 'option');
                                 ?>
-                                <div class="titem text-center col-md-6">
-                                    <div style="border-bottom: 2px solid #312884; margin-bottom: 5em;">
+                                <div class="titem text-center col-md-6" style="display: flex; flex-direction: column;">
+                                    <div style="border-bottom: 2px solid #312884; margin-bottom: 5em; flex-grow: 1;">
                                       <div class="team-img" style="border: 0.6em solid; border-color: rgba(48, 41, 125, 0.17);">
                                           <img src="<?php echo $picture_teammember ?>" alt="">
                                           <img class="disabled" src="<?php echo $picture_teammember_colored ?>" alt="">
                                       </div>
-                                      <div class="info">
+                                      <div class="info" style="height: 100%;">
                                           <h6><?php echo $name ?></h6>
                                           <span><?php echo $position_title ?></span>
-                                          <p><?php echo $description ?></p>
-                                          <div class="social">
-                                              <?php if( have_rows('social',  'options')):
-                                                  while ( have_rows('social',  'options')) : the_row();
-                                                  $linkedin = get_sub_field('linkedin', 'option');
-                                                  $telephone = get_sub_field('telephone', 'option');
-                                                  $mobile = get_sub_field('mobile', 'option');
-                                                  $email = get_sub_field('email', 'option');
-                                                  $vcard = get_sub_field('vcard', 'option');
-                                                  ?>
-                                                  <br>
-                                                  <div class="contact-wrapper">
-                                                    <div class="contact-ph-wrapper">
-                                                      <p class="contact-ph">T: <?php echo $telephone ?></p>
-                                                      <?php
-                                                        if ($name !== 'Caroline Devlin') {
-                                                          echo '<p class="contact-ph">M: '.$mobile.'</p>';
-                                                        }
-                                                      ?>
+                                          <div style="display: flex; flex-direction: column; justify-content: space-between; height: 100%;">
+                                            <p><?php echo $description ?></p>
+                                            <div class="social">
+                                                <?php if( have_rows('social',  'options')):
+                                                    while ( have_rows('social',  'options')) : the_row();
+                                                    $linkedin = get_sub_field('linkedin', 'option');
+                                                    $telephone = get_sub_field('telephone', 'option');
+                                                    $mobile = get_sub_field('mobile', 'option');
+                                                    $email = get_sub_field('email', 'option');
+                                                    $vcard = get_sub_field('vcard', 'option');
+                                                    ?>
+                                                    <!-- <br> -->
+                                                    <div class="contact-wrapper">
+                                                      <div class="contact-ph-wrapper">
+                                                        <p class="contact-ph">T: <?php echo $telephone ?></p>
+                                                        <?php
+                                                          if ($name !== 'Caroline Devlin') {
+                                                            echo '<p class="contact-ph">M: '.$mobile.'</p>';
+                                                          }
+                                                        ?>
+                                                      </div>
+                                                      <div class="contact-social-wrapper">
+                                                        <a class="contact-social" href="<?php echo $vcard ?>"><img src="/lansdowne/wp-content/themes/html5blank2/images/vCard-button.png" /></a>
+                                                        <a class="contact-social" href="<?php echo $linkedin ?>"><img src="/lansdowne/wp-content/themes/html5blank2/images/linkedin-button.png" /></a>
+                                                      </div>
                                                     </div>
-                                                    <div class="contact-social-wrapper">
-                                                      <a class="contact-social" href="<?php echo $vcard ?>"><img src="/lansdowne/wp-content/themes/html5blank2/images/vCard-button.png" /></a>
-                                                      <a class="contact-social" href="<?php echo $linkedin ?>"><img src="/lansdowne/wp-content/themes/html5blank2/images/linkedin-button.png" /></a>
-                                                    </div>
-                                                  </div>
-                                                  <!-- <a href="mailto:<?php echo $email ?>"></a> -->
-                                              <?php endwhile;
-                                                  else :
-                                                      // no rows found
-                                                  endif;
-                                              ?>
+                                                    <!-- <a href="mailto:<?php echo $email ?>"></a> -->
+                                                <?php endwhile;
+                                                    else :
+                                                        // no rows found
+                                                    endif;
+                                                ?>
+                                            </div>
                                           </div>
                                       </div>
                                     </div>
