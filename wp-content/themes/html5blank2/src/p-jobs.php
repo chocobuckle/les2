f7ca2ebb0f1a31e0ab23540a7a51f646bd0db898
Conor Doyle <contact@conordoyle.io>
2018-07-20 11:10:44 +0100

    Tweak jobs slider so that all entries are the same height

--- wp-content/themes/html5blank2/src/p-jobs.php
+++ wp-content/themes/html5blank2/src/p-jobs.php
@@ -16,43 +16,28 @@
                     </div>
 
                     <div class="owl-carousel owl-theme">
-                                        <?php
+                      <?php
+                        if( have_rows('jobs', 'options') ):
+                            while ( have_rows('jobs', 'options') ) : the_row(); ?>
+                            <div class="col-lg-12" style="margin-bottom: 3em;">
+                                <div class="item text-center mb-md50">
+                                <a href="<?php the_sub_field('file'); ?>" target="_blank">
+                                    <div class="post-img">
+                                        <img src="<?php the_sub_field('job_image'); ?>" alt="">
+                                    </div>
+                                    <div class="content">
+                                        <h5><?php the_sub_field('job_title'); ?></h5>
+                                    </div>
+                                </a>
+                                </div>
+                            </div>
 
-                                        if( have_rows('jobs', 'options') ):
+                                <?php
 
-                                            while ( have_rows('jobs', 'options') ) : the_row(); ?>
-
-                                            <div class="col-lg-12" style="margin-bottom: 3em;">
-                                                <div class="item text-center mb-md50">
-                                                <a href="<?php the_sub_field('file'); ?>" target="_blank">
-                                                    <div class="post-img">
-                                                        <img src="<?php the_sub_field('job_image'); ?>" alt="">
-                                                        <!-- <div class="date">
-                                                            <a href="#0">
-                                                                <span>06</span>
-                                                                <span>Aug</span>
-                                                            </a>
-                                                        </div> -->
-                                                    </div>
-                                                    <div class="content">
-                                                        <!-- <span class="tag">
-                                                            <a href="#0">WordPress</a>
-                                                        </span> -->
-                                                        <h5><?php the_sub_field('job_title'); ?></h5>
-                                                        <!-- <p>Lorem Ipsum is simply dummy text of the printing and type setting industry. Lorem Ipsum has been the dummy text ever</p> -->
-                                                    </div>
-                                                </a>
-                                                </div>
-                                            </div>
-
-                                                <?php
-
-                                            endwhile;
-
-                                        else :
-                                        endif;
-
-                                        ?>
+                            endwhile;
+                          else :
+                        endif;
+                      ?>
                     </div>
                 </div>
             </div>
