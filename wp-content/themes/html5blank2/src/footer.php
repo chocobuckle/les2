ef7f9224f6e61608ff54f7a9b8ef8aeb33c64665
Conor Doyle <contact@conordoyle.io>
2018-07-20 15:32:11 +0100

    Privacy policy update

--- wp-content/themes/html5blank2/src/footer.php
+++ wp-content/themes/html5blank2/src/footer.php
@@ -1,4 +1,4 @@
-  
+
         <!-- =====================================
     	==== Start Footer -->
     	<footer class="text-center">
@@ -6,17 +6,36 @@
 
                 <!-- Logo -->
                 <a class="logo" href="#">
-                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.svg" alt="logo">            
+                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.svg" alt="logo">
                 </a>
-                
+
                 <div class="social">
                     <!-- <a href="#0"><i class="icofont icofont-brand-linkedin"></i></a> -->
                 </div>
 
                 <p>&copy; 2018 Lansdowne. All Rights Reserved.</p>
-                <a style="font-size:14px" target="_blank" href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/PrivacyNotice.pdf">Privacy Policy</a>
-
             </div>
+
+            <div class="modal-page">
+              <div class="modal-page__container">
+                <a href="#popup-article" style="font-size:14px" class="open-popup">Privacy Policy</a>
+              </div>
+            </div>
+            <div id="popup-article" class="popup" style="position: relative; z-index: 1000;">
+              <div class="popup__block">
+                <h1 class="popup__title">The my adventure in the France and photography with Tour De France</h1>
+                <p class="modal__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam eaque optio vitae in explicabo recusandae sit id sapiente excepturi tempore, nemo, nulla odio deleniti rerum nisi perferendis aut molestias! Incidunt nesciunt iusto praesentium! In at maiores quibusdam enim quis, quam!</p>
+                <p class="modal__text">Architecto magni dolores cumque tenetur nemo id aperiam, ratione temporibus at, consectetur totam, fuga et illo rerum earum dicta. Vitae ullam harum enim aliquid hic commodi voluptas cumque iste ex accusantium architecto doloremque reprehenderit, asperiores vero dolor, esse inventore dolorem.</p>
+                <p class="modal__text">Excepturi eaque, reprehenderit dolores, cum molestias repellendus in expedita. Placeat totam, quos pariatur quidem explicabo id harum ab voluptatum. Necessitatibus, aliquam! Dolorum voluptatem ut laudantium excepturi cumque, hic iure impedit ullam accusantium error natus recusandae, quia fuga quo voluptates officiis?</p>
+                <p class="modal__text">Dignissimos debitis eos necessitatibus accusantium natus voluptates illo blanditiis corporis minus. Repudiandae libero quae, illo error expedita consectetur possimus voluptate eum esse quam molestiae tempore dignissimos ipsam similique ab quod. Ea earum adipisci rem voluptatem aliquid voluptatum deleniti necessitatibus provident.</p>
+                <p class="modal__text">Dicta eum amet impedit maiores accusamus numquam saepe necessitatibus temporibus ut! Velit ducimus repellendus fuga repudiandae culpa voluptatibus delectus praesentium totam odit ratione, tenetur assumenda, labore esse et nostrum a, aut veritatis. Nihil, voluptas, impedit? Magnam dolorum, iure repellendus vitae.</p>
+                <p class="modal__text">Temporibus voluptatum voluptatibus iste, nam atque dignissimos quam labore sequi adipisci tempore exercitationem quos, libero, reprehenderit facere quasi soluta, itaque at eum cum possimus! Facilis, tempora soluta at quis. Nemo expedita voluptate esse nam ex odit, sequi eveniet quibusdam, dolores?</p>
+                <p class="modal__text">Praesentium laboriosam iste dolore cumque voluptatibus deleniti quia, delectus provident, illum aperiam, atque molestiae. Cum delectus, doloribus expedita eius veritatis assumenda deleniti veniam reprehenderit animi ut, eaque asperiores, dicta incidunt omnis repellendus dolorum enim inventore rerum voluptatem saepe error id.</p>
+                <a href="#" class="popup__close" style="text-decoration: none;">X</a>
+              </div>
+            </div>
+
+
         </footer>
     	<!-- End Footer ====
     	======================================= -->
