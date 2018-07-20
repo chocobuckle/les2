3024f1f376c1d51a92ce3d772dc52e29c7f05ac9
Conor Doyle <contact@conordoyle.io>
2018-07-20 16:14:35 +0100

    Add privacy policy

--- wp-content/themes/html5blank2/src/footer.php
+++ wp-content/themes/html5blank2/src/footer.php
@@ -23,14 +23,104 @@
             </div>
             <div id="popup-article" class="popup" style="position: relative; z-index: 1000;">
               <div class="popup__block">
-                <h1 class="popup__title">The my adventure in the France and photography with Tour De France</h1>
-                <p class="modal__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam eaque optio vitae in explicabo recusandae sit id sapiente excepturi tempore, nemo, nulla odio deleniti rerum nisi perferendis aut molestias! Incidunt nesciunt iusto praesentium! In at maiores quibusdam enim quis, quam!</p>
-                <p class="modal__text">Architecto magni dolores cumque tenetur nemo id aperiam, ratione temporibus at, consectetur totam, fuga et illo rerum earum dicta. Vitae ullam harum enim aliquid hic commodi voluptas cumque iste ex accusantium architecto doloremque reprehenderit, asperiores vero dolor, esse inventore dolorem.</p>
-                <p class="modal__text">Excepturi eaque, reprehenderit dolores, cum molestias repellendus in expedita. Placeat totam, quos pariatur quidem explicabo id harum ab voluptatum. Necessitatibus, aliquam! Dolorum voluptatem ut laudantium excepturi cumque, hic iure impedit ullam accusantium error natus recusandae, quia fuga quo voluptates officiis?</p>
-                <p class="modal__text">Dignissimos debitis eos necessitatibus accusantium natus voluptates illo blanditiis corporis minus. Repudiandae libero quae, illo error expedita consectetur possimus voluptate eum esse quam molestiae tempore dignissimos ipsam similique ab quod. Ea earum adipisci rem voluptatem aliquid voluptatum deleniti necessitatibus provident.</p>
-                <p class="modal__text">Dicta eum amet impedit maiores accusamus numquam saepe necessitatibus temporibus ut! Velit ducimus repellendus fuga repudiandae culpa voluptatibus delectus praesentium totam odit ratione, tenetur assumenda, labore esse et nostrum a, aut veritatis. Nihil, voluptas, impedit? Magnam dolorum, iure repellendus vitae.</p>
-                <p class="modal__text">Temporibus voluptatum voluptatibus iste, nam atque dignissimos quam labore sequi adipisci tempore exercitationem quos, libero, reprehenderit facere quasi soluta, itaque at eum cum possimus! Facilis, tempora soluta at quis. Nemo expedita voluptate esse nam ex odit, sequi eveniet quibusdam, dolores?</p>
-                <p class="modal__text">Praesentium laboriosam iste dolore cumque voluptatibus deleniti quia, delectus provident, illum aperiam, atque molestiae. Cum delectus, doloribus expedita eius veritatis assumenda deleniti veniam reprehenderit animi ut, eaque asperiores, dicta incidunt omnis repellendus dolorum enim inventore rerum voluptatem saepe error id.</p>
+                <h1 class="popup__title">Privacy Notice (May 2018)</h1>
+                <p class="modal__text__bold">Company Description: </p>
+                <p class="modal__text">Lansdowne Executive Search Limited is an Executive Search company that provides recruitment and
+HR consultancy Services to candidates and to client companies.</p>
+                <p class="modal__text">Office: Landsowne Executive Search, 13 Priory Office Park, Stillorgan, Co. Dublin, A94 Y959, Ireland. </p>
+                <p class="modal__text">Email: enquiries@lansdownesearch.ie</p>
+                <p class="modal__text">Phone: +353 1 549 0066 </p>
+                <p class="modal__text__bold">Personal Data</p>
+                <p class="modal__text">
+                  We collect relevant Personal Data that you provide to us for the purposes that are set out in this
+                  Privacy Notice. This Personal Data may include your name, address, phone number, email address,
+                  relevant remuneration information, curriculum vitae and links to your professional profiles available
+                  in the public domain. Where appropriate, and with your consent, we may also collect other
+                  information such as references (to verify your experience and qualifications). This data is used, as
+                  appropriate, to fulfil our services to clients and candidates.
+                </p>
+                <p class="modal__text">
+                  The legal basis for processing your personal information is that you have given your consent to us.
+                  This consent can be given at the time of initially seeking our services or having read the company’s
+                  Privacy Notice and providing your consent to our continued use of the personal data you have
+                  supplied us.
+                </p>
+                <p class="modal__text__bold">How we use Personal Data </p>
+                <p class="modal__text">
+                  Any personal data you provide will be used for the purpose for which you supplied it and will be
+                  stored confidentiality and securely, in accordance with General Data Protection Regulations (GDPR).
+                  Your information will be used for matching your skills with potential employment opportunities and
+                  will only be disclosed to prospective employers where you have given your prior consent.
+                </p>
+                <p class="modal__text">By providing your personal information, and with your agreement, we may use the information to: </p>
+                <ul class="modal__text">
+                  <li class="modal__text">Notify you of potentially suitable employment opportunities </li>
+                  <li class="modal__text">Seek your views on our services and on potential improvements </li>
+                </ul>
+                <p class="modal__text__bold">Retention Period </p>
+                <p class="modal__text">
+                  Based on industry norms, we will keep your personal data for 5 years from the date you give us
+                  consent to retain it. You have the right to withdraw your consent and have your personal
+                  information erased at any time and we will respond to such requests within 10 working days.
+                </p>
+                <p class="modal__text__bold">Website usage information </p>
+                <p class="modal__text">
+                  When using our website, we may collect information about your computer browser type and
+                  operating system and your Internet Protocol (IP) addresses. We collate this information to
+                  understand how our website is used so that we can improve and expand the services we offer. The
+                  aggregated data collected does not identify individual users. This information typically includes:
+                </p>
+                <ul class="modal__text">
+                  <li class="modal__text">Number of visitors to each page of our website</li>
+                  <li class="modal__text">Total number of visits to our website</li>
+                  <li class="modal__text">Domain names of our visitors’ internet service providers</li>
+                </ul>
+                <p class="modal__text__bold">Security</p>
+                <p class="modal__text">
+                  Lansdowne Executive Search Ltd. takes appropriate measures to protect your personal data, in
+                  accordance with GDPR. Any third-party service providers that we engage are required to use
+                  appropriate measures to protect the confidentiality and security of information. We use a variety of
+                  security technologies and procedures to help prevent unauthorised use of Personal Data. No
+                  transmission of information via the Internet or a mobile phone network connection is fully secure
+                  and therefore we cannot guarantee the security of information during transmission.
+                </p>
+                <p class="modal__text__bold">Links to other websites</p>
+                <p class="modal__text">
+                  Our website may provide links to third party websites. This Privacy Notice does not apply to these
+                  websites. If you opt to enter a linked website, you agree that we shall not be liable, directly or
+                  indirectly, for:
+                </p>
+                <ul class="modal__text">
+                  <li class="modal__text">The content of such websites</li>
+                  <li class="modal__text">The use that others make of these websites</li>
+                  <li class="modal__text">How these websites use your Personal Data</li>
+                </ul>
+                <p class="modal__text">
+                  It is therefore your responsibility to check the legal and privacy statements posted on each website
+                  that you access before entering any Personal Data.
+                </p>
+                <p class="modal__text__bold">Cookies</p>
+                <p class="modal__text">
+                  Our website and mobile applications may use technology called ͞cookies͟. A cookie is a small file
+                  that is placed on your device by a server. Cookies enable web applications to respond to and adjust
+                  to your needs and preferences by gathering information about your preferences.
+                </p>
+                <p class="modal__text">
+                  You can choose to accept or reject cookies. If you choose to reject cookies, you may not get the
+                  benefit of all features of our website. Please refer to your Internet browser’s instructions or help
+                  screen to learn more about these functions and to specify your cookie preferences.
+                </p>
+                <p class="modal__text__bold">Disclosure to third parties</p>
+                <p class="modal__text">
+                  We may disclose your Personal Data to contracted companies who may act as ͞data processors͟ on
+                  our behalf. Our data processors may include consultants, technical support providers and ERP
+                  companies. We share information with contracted third parties only for the purposes of assisting
+                  with the delivery of our services.
+                </p>
+                <p class="modal__text">
+                  Our providers of support services are prohibited from using the personal information we provide for
+                  their own purposes.
+                </p>
                 <a href="#" class="popup__close" style="text-decoration: none;">X</a>
               </div>
             </div>
